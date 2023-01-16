4e2d8df02df5682d18ba4d778656c141c6940690
neoSteelrain <magnosix6@gmail.com>
2023-01-16 22:25:28 +0900

    feat : 유튜브 API Client - search.list 요청의 setFields 설정시 응답에 페이징정보가 안넘어오는 문제 해결

--- src/main/java/com/steelrain/lilac/batch/youtube/YoutubeDataV3Client.java
+++ src/main/java/com/steelrain/lilac/batch/youtube/YoutubeDataV3Client.java
@@ -1,16 +1,17 @@
 package com.steelrain.lilac.batch.youtube;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.steelrain.lilac.batch.config.APIConfig;
 import com.steelrain.lilac.batch.exception.LilacYoutubeAPIException;
 
 import java.io.IOException;
-import java.util.List;
+import java.security.GeneralSecurityException;
 
 public class YoutubeDataV3Client implements IYoutubeClient{
 
@@ -20,59 +21,31 @@ public class YoutubeDataV3Client implements IYoutubeClient{
         this.m_apiConfig = apiConfig;
     }
 
+   /* public DateTime getDateTimeTest(){
+        return DateTime.parseRfc3339(String.format("%d-01-01T00:00:00Z",LocalDateTime.now().getYear()));
+    }*/
 
     @Override
     public SearchListResponse getYoutubePlayList(String keyword) {
-        /*
-        - 검색 작업순서
-        1. 미리등록된 키워드 (subject, license)로 Search.list 메서드를 호출한다
-        2. 결과중에서 playlistId 가 있는 영상만 추출한다
-        3. 추출된 영상의 playlistId 를 가지고 PlaylistItem 메서드로 해당 재생목록의 모든 영상을 가져온다
-        4. 재생목록에 속한 영상들의 코멘트들을 전부 가져온다
-        5. 코멘트들에 감정분석을 적용해서 긍정수치가 70% 이상(또는 부정보다는 긍정이 높은) 이라면 영상정보, 재생목록정보, 채널정보를 DB에 저장한다
-
-        # 사용자에게 제공하는 검색서비스는 DB에 저장된 재생목록의 제목을 대상으로 하거나, 개별적인 영상들의 제목을 검색하게 된다.
-        
-        - DB 저장순서
-        1. 재생목록정보
-        2. 채널정보
-        3. 영상정보
-        4. 코멘트정보
-         */
-        return searchYoutubePlayList(keyword);
-    }
-
-    private SearchListResponse searchYoutubePlayList(String keyword){
         SearchListResponse response = null;
-        try{
-            // 유튜브 DATA V3 API 객체 생성
-            YouTube youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
+        try {
+            YouTube youtube = new YouTube.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), new HttpRequestInitializer() {
                 public void initialize(HttpRequest request) throws IOException {
                 }
             }).setApplicationName("lilac").build();
 
-            // 키워드, 요청파라미터 설정
-            YouTube.Search.List search = youtube.search().list("id,snippet");
-            search.setKey(m_apiConfig.getYoutubeKey());
-            search.setQ(keyword);
-            //search.setType("video,channel,playlist");
-            search.setType("playlist,video");
-            search.setMaxResults(50L); // API 1 call 당 최대로 가져올 수 있는 영상의 갯수는 50개
-            search.setOrder("viewCount");
-            search.setVideoDefinition("high");
-            search.setVideoEmbeddable("any");
+            YouTube.Search.List request = youtube.search().list("id,snippet");
+            request.setQ(keyword)
+            .setKey(m_apiConfig.getYoutubeKey())
+            .setType("playlist")
+            .setMaxResults(50L)
+            //.setOrder("date")
+            .setPublishedAfter(DateTime.parseRfc3339("2022-01-01T00:00:00Z")) // The value is an RFC 3339 formatted date-time value (1970-01-01T00:00:00Z).
+            .setFields("items(id/kind,id/playlistId,snippet/channelId,snippet/thumbnails/high/url,snippet/title,snippet/publishedAt,snippet/description,snippet/channelTitle),nextPageToken,pageInfo");
 
+            response = request.execute();
 
-            // 응답필드 설정
-            //search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
-            //search.setFields("items(id/kind,id/videoId,id/playlistId,snippet/title,snippet/thumbnails/high/url,snippet/publishedAt,snippet/description)");
-            //search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
-            //search.setFields("items(id/kind,id/videoId,id/playlistId,id/channelId,snippet/thumbnails/high/url,snippet/title/publishedAt/description/channelTitle)");
-            response = search.execute(); // 유튜브 DATA V3 API 호출
-            /*System.out.println("========================");
-            search.getLastResponseHeaders().values().stream().forEach(obj -> System.out.println(obj.toString()));
-            System.out.println("========================");*/
-        }catch(Exception e){
+        } catch (IOException | GeneralSecurityException e) {
             throw new LilacYoutubeAPIException("유튜브 API호출 도중 예외 발생", e);
         }
         return response;
--- src/test/java/com/steelrain/lilac/batch/youtube/YoutubeTests.java
+++ src/test/java/com/steelrain/lilac/batch/youtube/YoutubeTests.java
@@ -5,10 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.steelrain.lilac.batch.config.APIConfig;
-import io.micrometer.core.instrument.util.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,37 +23,15 @@ public class YoutubeTests {
     private APIConfig apiConfig;
 
 
-
     @Test
-    public void testgGetYoutubePlayList(){
-        YoutubeDataV3Client client = new YoutubeDataV3Client(apiConfig);
-        SearchListResponse res = client.getYoutubePlayList("정보처리기사 2022");
-
-        searchListResponseToJsonFile(res);
+    public void testGetYoutubePlayList(){
+        IYoutubeClient youtubeClient = new YoutubeDataV3Client(this.apiConfig);
+        SearchListResponse res = youtubeClient.getYoutubePlayList("정보처리기사");
 
         assertThat(res != null);
-    }
 
-    @Test
-    public void testGetYoutubePlayListV2(){
-        YoutubeClientV2 v2 = new YoutubeClientV2(apiConfig);
-        SearchListResponse res = v2.getYoutubePlayList("정보처리기사 2023");
         searchListResponseToJsonFile(res);
     }
-
-   /* @Test
-    public void testGoggleDateTime(){
-        YoutubeClientV2 v2 = new YoutubeClientV2(apiConfig);
-        DateTime dt = v2.getDateTimeTest();
-        String tmp = dt.toStringRfc3339();
-
-        assertThat(!StringUtils.isEmpty(tmp));
-
-        System.out.println("==============================");
-        System.out.println("tmp : " + tmp);
-        System.out.println("==============================");
-    }*/
-
 
 
     private void searchListResponseToJsonFile(SearchListResponse response){
