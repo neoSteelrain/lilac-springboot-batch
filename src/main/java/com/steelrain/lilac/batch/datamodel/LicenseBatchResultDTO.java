014ecd45102ada03257e1529cb1002742f1d2168
neoSteelrain <magnosix6@gmail.com>
2023-01-25 02:49:46 +0900

    feat : 커밋 체크에서 빠진 화일 다시 커밋

---
+++ src/main/java/com/steelrain/lilac/batch/datamodel/LicenseBatchResultDTO.java
@@ -0,0 +1,9 @@
+package com.steelrain.lilac.batch.datamodel;
+
+import java.sql.Timestamp;
+
+public class LicenseBatchResultDTO {
+    private int id;
+    private String pageToken;
+    private Timestamp updateTime;
+}
